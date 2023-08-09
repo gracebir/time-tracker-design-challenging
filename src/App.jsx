2f4fa82e7610c0d4b78c2d92cd9b93d5449c6019
gracebir <grace.devolop@gmail.com>
2023-08-09 17:50:55 +0200

    assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-
+import data from './data.json'
 
 function App() {
-  return <>Hi</>
+  return (
+    <div>
+      <h1 className="text-xl font-bold">Hello world</h1>
+      
+    </div>
+  )
 }
 
 export default App
