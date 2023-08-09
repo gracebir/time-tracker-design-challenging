6eef44f2bac59204f14d75eaa45da34e003ceb44
gracebir <grace.devolop@gmail.com>
2023-08-09 18:03:27 +0200

    get colors defined

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "blue-color":"var(--color-blue)",
+        "work-color":"var(--light-red-work)",
+        "play-color":"var(--soft-blue-play)",
+        "study-color":"var(--light-red-study)",
+        "exercise-color":"var(--lime-green-exercise)",
+        "social-color":"var(--violet-social)",
+        "care-color":"var(--soft-orange-self-care)",
+        "very-dark":"var(--very-dark-blue)",
+        "dark-blue":"var(--dark-blue)",
+        "desaturated-blue":"var(--desaturated-blue)",
+        "pale-blue":"var(--pale-Blue)"
+      }
+    },
   },
   plugins: [],
 }
