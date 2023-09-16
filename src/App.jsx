5730f3c86bf186bc68c41059bb2e2c56af15abe5
gracebir <grace.devolop@gmail.com>
2023-09-16 14:42:28 +0200

    main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import ProfileCard from './components/ProfileCard'
+import TimeCrak from './components/TimeCrak'
 import data from './data.json'
 
 function App() {
+  console.log(data)
   return (
-    <div>
-      <h1 className="text-xl font-bold">Hello world</h1>
-      
+    <div className='text-gray-200 font-rubik w-full bg-very-dark flex items-center min-h-screen'>
+      <div className='max-w-7xl grid grid-cols-1 gap-8 lg:grid-cols-4 flex-1 mx-auto min-h-[55vh]'>
+        <ProfileCard />
+        <div className='lg:col-span-3 col-span-1 grid grid-cols-1 lg:grid-cols-3 gap-8'>
+          <TimeCrak />
+          <TimeCrak />
+          <TimeCrak />
+          <TimeCrak />
+          <TimeCrak />
+          <TimeCrak />
+        </div>
+      </div>
     </div>
   )
 }
