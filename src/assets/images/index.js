1fc23c6241eac2a8e4230ce4e44eba2814cb742c
gracebir <grace.devolop@gmail.com>
2023-09-16 14:57:15 +0200

    export assets

---
+++ src/assets/images/index.js
@@ -0,0 +1,15 @@
+import player from './icon-play.svg'
+import selfCare from './icon-self-care.svg'
+import social from './icon-social.svg'
+import study from './icon-study.svg'
+import work from './icon-work.svg'
+import exercise from './icon-exercise.svg'
+
+export {
+    player,
+    selfCare,
+    social,
+    study,
+    work,
+    exercise
+}
