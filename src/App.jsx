efeabd039effc11172de0eec47f05ae9da001638
gracebir <grace.devolop@gmail.com>
2023-09-16 16:11:05 +0200

    pass timeframe into prop

--- src/App.jsx
+++ src/App.jsx
@@ -23,13 +23,13 @@ function App() {
   return (
     <div className='text-gray-200 font-rubik w-full bg-very-dark flex items-center min-h-screen'>
       <div className='max-w-7xl grid grid-cols-1 gap-8 lg:grid-cols-4 flex-1 mx-auto min-h-[55vh]'>
-        <ProfileCard setTimeframe={setTimeframe} />
+        <ProfileCard timeframe={timeframe} setTimeframe={setTimeframe} />
         <div className='lg:col-span-3 col-span-1 grid grid-cols-1 lg:grid-cols-3 gap-8'>
           {times.map((time, index) => (
             <TimeCrak
               bgColor={staticTimeAssets[index].bgColor}
               icon={staticTimeAssets[index].icon}
-              key={time.title}
+              key={index}
               current={time.current}
               previous={time.previous}
               timeFrame={timeframe}
