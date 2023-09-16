af908fa7893438777c554f9c6e614dc3b21b6b4f
gracebir <grace.devolop@gmail.com>
2023-09-16 15:47:28 +0200

    static data

---
+++ src/utils/data.js
@@ -0,0 +1,34 @@
+import {work, player, study, exercise, social, selfCare} from '../assets/images/index'
+
+export const staticTimeAssets = [
+    {
+        title:"Work",
+        icon: work,
+        bgColor: "bg-work-color"
+    },
+    {
+        title:"Play",
+        icon: player,
+        bgColor: "bg-play-color"
+    },
+    {
+        title:"study",
+        icon: study,
+        bgColor: "bg-study-color"
+    },
+    {
+        title:"Exercise",
+        icon: exercise,
+        bgColor: "bg-exercise-color"
+    },
+    {
+        title:"Social",
+        icon: social,
+        bgColor: "bg-social-color"
+    },
+    {
+        title:"Self Care",
+        icon: selfCare,
+        bgColor: "bg-care-color"
+    }
+]
